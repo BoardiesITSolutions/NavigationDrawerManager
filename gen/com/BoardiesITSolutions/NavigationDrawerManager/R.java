8d533540a8cf22fc469172344b66911e0f2393d1
Chris Board <hello@chrisboard.co.uk>
2014-03-24 21:29:01 +0000

    Resolved issue with Generic OnItemClickListener
    Resolved issue with Generic OnItemClickListener not being triggered on
    menu item click

--- gen/com/BoardiesITSolutions/NavigationDrawerManager/R.java
+++ gen/com/BoardiesITSolutions/NavigationDrawerManager/R.java
@@ -13,18 +13,25 @@ public final class R {
     public static final class drawable {
         public static int ic_drawer=0x7f020000;
         public static int ic_launcher=0x7f020001;
+        public static int ic_menu_block=0x7f020002;
+        public static int ic_menu_blocked_user=0x7f020003;
     }
     public static final class id {
-        public static int row_edittext=0x7f060003;
+        public static int row_button=0x7f060006;
+        public static int row_button_borderless=0x7f060007;
+        public static int row_edittext=0x7f060005;
         public static int row_header=0x7f060001;
         public static int row_icon=0x7f060000;
-        public static int row_spinner=0x7f060004;
-        public static int row_switch=0x7f060005;
-        public static int row_textview=0x7f060002;
+        public static int row_spinner=0x7f060008;
+        public static int row_subHeader=0x7f060002;
+        public static int row_subTextView=0x7f060003;
+        public static int row_switch=0x7f060009;
+        public static int row_textview=0x7f060004;
     }
     public static final class layout {
-        public static int row_menu_item=0x7f030000;
-        public static int spinner=0x7f030001;
+        public static int drawer_list_item=0x7f030000;
+        public static int row_menu_item=0x7f030001;
+        public static int spinner=0x7f030002;
     }
     public static final class string {
         public static int app_name=0x7f040000;
